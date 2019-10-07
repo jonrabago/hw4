f044af397d7f60585308d35f1f090e6deac7979a
jonrabago <55300118+jonrabago@users.noreply.github.com>
2019-10-07 12:15:23 -0700

    Update bigger-margins.js

--- bigger-margins.js
+++ bigger-margins.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
   background(255);
 
-  for (var x = 0; x < width+10; x = x + 10) {
+  for (var x = 100; x < width-100; x = x + 10) {
     line(x, height/2, mouseX, mouseY);
   }
 }
